dc4a9217c593b4317d2ab098a7de6d5f60ab2501
Evan Losh <evand.losh@gmail.com>
2023-11-25 10:38:28 -0500

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -4,7 +4,7 @@ import React from "react"
 function Keypad() {
     return (
         <div>
-            <input type="password" onChange={console.log('Entering password...')}></input>
+            <input type="password" onChange={() => console.log('Entering password...')}></input>
         </div>
     )
 }
